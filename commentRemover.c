5dc8c8954aa3ce52a372b48caef152b2b01140ed
fayzRahman <161078849+Fayz-Rahman@users.noreply.github.com>
2025-10-06 21:57:38 +0000

    feat: Add validation for input file argument

--- commentRemover.c
+++ commentRemover.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+
+bool is_input_file_provided(int argc){
+    if(argc<2){
+        return false;
+    }
+
+    return true;
+}
+
+
 
 int main(int argc , char* argv[]){
+
+    if(!is_input_file_provided(argc)){
+        printf("no input file is provided\nUsage: %s <filename>\n",argv[0]);
+        return 1;
+    }
+
+
 
     return 0;
 }
